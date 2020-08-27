edc4d218e6cb7d451b7e8b9237db204d16bb61c2
timmyCoder <olatoyedan@gmail.com>
2020-08-27 03:28:50 +0100

    added header

--- app/src/main/java/com/example/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/MainActivity.kt
@@ -4,30 +4,35 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
-import androidx.ui.tooling.preview.Preview
+import androidx.ui.layout.Column
+import androidx.ui.layout.Height
+import androidx.ui.layout.Spacing
+import androidx.ui.res.imageResource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun NewsStory() {
+    val image = imageResource(R.drawable.header)
+    Column(
+        modifier = Spacing(16.dp)
+    ) {
+        val imageModifier = Height(180.dp)
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
+        /*Image(image, modifier = imageModifier,
+                contentScale = ContentScale.Crop)*/
+
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
     }
 }
